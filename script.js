233b145b27ef39d4e9c347f8c2e239dbbf8fab30
Matthew Richardson <matthew_richardson@ymail.com>
2025-09-01 20:44:40 +0100

    Started todo list for javascript as comments

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Todo list:
+
+(BASIC START)
+
+-On click event listener needed for when clicking burger menu
+
+-Burger menu open and drop down menu with height + width.
+
+-Burger menu drop down menu needs links for navigation buttons
+
+-On hover when burger menu pressed
+
+(BASIC END)
+
+
+
+
+*/
