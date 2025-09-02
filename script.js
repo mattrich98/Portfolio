7e0529769253955a363412a34b4d2a24f64cb351
Matthew Richardson <matthew_richardson@ymail.com>
2025-09-02 14:38:09 +0100

    changed close burger menu symbol x

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function burgerMenuOnClickToggle(){
 burgerMenu = !burgerMenu; //toggles between true and false
 
 if (burgerMenu === false){
-    document.getElementById("BurgerIcon").innerHTML = "!";
+    document.getElementById("BurgerIcon").innerHTML = "⤫";
     document.getElementById("barBox").style.opacity = 100; //dont need to add + "%" if it's added in CSS already
 } 
 else if (burgerMenu === true){
