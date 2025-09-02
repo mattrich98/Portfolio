6f95f26b0d319f2206fc405dec6e004de8099247
Matthew Richardson <matthew_richardson@ymail.com>
2025-09-02 08:20:19 +0100

    function plans simplified a little more

--- script.js
+++ script.js
@@ -33,9 +33,12 @@ Functions needed:
 
 ---Dropdown menu function on click, drops rectangle with specified dimensions and links on that, perhaps making a div somehow.
 
---May try and nest the navigation bar into itself, and make the menu appear on click, a lazy method of doing a burger menu
+---May try and nest the navigation bar into itself, and make the menu appear on click, a lazy method of doing a burger menu
 
-
+---Once the above idea is created, then I may try and experiment with making it more proper.
 
 That should be it for now, and I'll see how things go.
+
+Maybe replace the inner.html text of the nav bar letters with an Open Menu button that switches between the two instead, somehow find a way to add an image
+- although having it in words makes it easier to know what it is
 */
