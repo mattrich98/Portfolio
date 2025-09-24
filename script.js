6ce0568ec287dfdbc87f1134c21390988f3360f3
Matthew Richardson <matthew_richardson@ymail.com>
2025-09-24 06:58:41 +0100

    added education menu to burger menu but needs 2 clicks to close for some reason?

--- script.js
+++ script.js
@@ -25,10 +25,13 @@ if (educationMenu === false){
     document.getElementById("infoDiv").style.visibility = "visible";
     document.getElementById("subTopAbout").innerHTML = "Education";
     document.getElementById("infoDiv").innerHTML = "University of the Arts London, BA (Hons) Games Design<br>-<br>Sep 2017 - Jan 2018<hr>London Metropolitan University, Games Animation, Modelling and Effects BSc (Hons)<br>-<br>Sep 2018 - Jan 2019<hr>South Thames College, UAL Level 3 BTEC Extended Diploma in Creative Media and Technology, Games design<br>-<br>Sep 2015 - Jul 2017, Grade: Merit (Year 1+2)<hr>Rutlish School<br>-<br>2011 - 2015, GCSE English, Maths, IT (Grade C)";
+    document.getElementById("BurgerIcon").innerHTML = "≡";
+    document.getElementById("barBox").style.visibility = "hidden";
 } 
 else if (educationMenu === true){
     document.getElementById("educationSelect").innerHTML = "○";
     document.getElementById("infoDiv").style.visibility = "hidden";
     document.getElementById("subTopAbout").innerHTML = "About me";
+
 }
 }
