1af9d26c8f9bf07a3f611f2d6ecfcbbc9ff33c9c
Matthew Richardson <matthew_richardson@ymail.com>
2025-09-24 05:18:51 +0100

    removed javascript ideas

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ else if (burgerMenu === true){
     document.getElementById("barBox").style.visibility = "hidden";
 }
 }
-console.log("test");
-
 
 function educationToggle0(){
 educationMenu = !educationMenu;
@@ -32,88 +30,4 @@ else if (educationMenu === true){
     document.getElementById("infoDiv").style.visibility = "hidden";
     document.getElementById("subTopAbout").innerHTML = "About me";
 }
-    console.log("testing2")
 }
-console.log(educationToggle0())
-
-/*● <- toggle on click : ○ <- toggle off click */
-/*Display: flex/none may not work so may just change inner html of the about me divs text with javascript*/
-
-
-
-/*NEW TODOS REMINDER:
-
-About me shifting section on click circle to change info
-
- I will try to make a carousel menu with circle buttons you press.
-
- or maybe two buttons per section and split it down the middle, highlighted in different colours.
-
- What I may do is this: Add arrow button to side of about me blue part.
-  it scrolls past exp -> job exp - > contact and recycle button to return to start.
-
-It may need a click next label, or next section -> so its recognisable easily.
-
-the way this will work is -> for each section, the about me card will change to match other things.
-
-Maybe i'll add a label on the left to symbolise what section like my CV
-
-
-then I have projects on the bottom. That overtime will have a carousel.
-
-to write more information on the card, I may make it so the profile photo dissapears, then the about me
-card extends upwards, so I can write more information about specific sections
-
-
-NEW PLANS:
-
-Decided for the projects section, the <BK> and FWD>  button will just switch between sections,
-ill make images for the arrows saying next page and back somehow
-
-Similar to Roblox, my plan may be this:
-
--> I make a div (For the background), nest a div inside (the information), then use hidden/visibility css.
--> Make different designs for each section, like a power point slide show.
-
-
-NEW TODOS END REMINDER*/
-
-
-
-/*
-DESKTOP PLANS:
-
-If I resize the screen to 50%, it looks fairly decent, so maybe I can use a Meta tag for that
-
-*/
-
-
-
-
-/*
-
-Maybe with CSS I'll try to do a hover on the project buttons instead somehow, add margins on the left and right to hide the sides
-
-*/
-
-
-
-
-
-//OTHER TODO PROBABLY FINAL:
-
-/*
-For the project section, I plan to make a 9x9 square for each project, like viewing images on X/Twitter.
-
-
-*/
-//OTHER TODO PROBABLY END
-
-/*EXTRA TODO:
-
-Optimize at least security game website for mobile too.
-
-
-
-
-EXTRA TODO END*/
